db0429c3442e1bcda6de0c8ac7b7fee0bbcc0142
Max Smirnov <masynchin@gmail.com>
2022-04-23 09:19:41 +0300

    Add test with nested folder for `ExcludeGitignoredPaths`

--- tests/test_ndnt/test_paths.py
+++ tests/test_ndnt/test_paths.py
@@ -9,16 +9,27 @@ def test_files_paths():
         folder / "fake.py",
         folder / "ignored.py",
         folder / ".gitignore",
+        folder / "nested" / "fake.py",
+        folder / "nested" / "ignored.py",
     }
 
 
 def test_python_extension_paths():
     folder = Path("tests/fake_folder")
     python_paths = ExtensionPaths(FilesPaths(folder), ".py")
-    assert set(python_paths) == {folder / "fake.py", folder / "ignored.py"}
+    assert set(python_paths) == {
+        folder / "fake.py",
+        folder / "ignored.py",
+        folder / "nested" / "fake.py",
+        folder / "nested" / "ignored.py",
+    }
 
 
 def test_exclude_gitignored_paths():
     folder = Path("tests/fake_folder")
     paths = ExcludeGitignoredPaths(folder, folder / ".gitignore")
-    assert set(paths) == {folder / "fake.py", folder / ".gitignore"}
+    assert set(paths) == {
+        folder / "fake.py",
+        folder / ".gitignore",
+        folder / "nested" / "fake.py",
+    }
