8d0f914ad9acd8e564bfa5f5a14fc60a1bac28ee
Max Smirnov <masynchin@gmail.com>
2022-04-24 17:39:34 +0300

    Pin minimal python version

--- setup.py
+++ setup.py
@@ -34,11 +34,17 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Utilities",
     ],
-    python_requires=">=3.7",  # I am not sure about it
+    python_requires=">=3.6",  # I am not sure about it
     install_requires=get_install_requires(),
     entry_points={
         "console_scripts": [
